04af62869bc8f286d38c6b60f275c335a5af1235
H-ZeX <hzx20112012@gmail.com>
2019-04-06 14:48:03 +0800

    test(RBTTest): make the test more random

--- src/test/java/rbt/RBTTest.java
+++ src/test/java/rbt/RBTTest.java
@@ -9,72 +9,93 @@ import java.util.function.Supplier;
 public class RBTTest {
     private Random random = new Random();
 
-    /**
-     * @return [rbt, dataArray]
-     */
-    private <T> Object[] render(int size, Class<T> dataClazz, Supplier<T> dataSupplier, Comparator<T> dataCmp) {
+    private <T> T[] renderData(int size, Class<T> dataClazz, Supplier<T> dataSupplier) {
         @SuppressWarnings("unchecked")
         T[] data = (T[]) Array.newInstance(dataClazz, size);
         for (int i = 0; i < size; i++) {
             data[i] = dataSupplier.get();
         }
-        Arrays.sort(data, dataCmp);
-
-        RBT<T> rbt = new RBT<>(dataCmp);
-        for (int i = 0; i < size; i++) {
-            rbt.insert(data[i]);
-        }
-        return new Object[]{rbt, data};
+        return data;
     }
 
     private <T> void test(Class<T> dataClazz, Supplier<T> dataSupplier, Comparator<T> dataCmp) {
         final int testCnt = 1024;
         final int maxSize = 1024;
         final int minSize = 10;
+        final int roundCnt = 128;
 
+        class Container {
+            private T data;
+            private long id;
+
+            public Container(T data, long id) {
+                this.data = data;
+                this.id = id;
+            }
+        }
         for (int i = 0; i < testCnt; i++) {
-            Object[] testData = render(random.nextInt(maxSize - minSize) + minSize, dataClazz, dataSupplier, dataCmp);
-            @SuppressWarnings("unchecked")
-            RBT<T> rbt = (RBT<T>) testData[0];
-            @SuppressWarnings("unchecked")
-            T[] data = (T[]) testData[1];
-
-            ArrayList<Integer> delInd = new ArrayList<>();
+            ArrayList<T> delList = new ArrayList<>();
+            ArrayList<T> addList = new ArrayList<>();
+            T[] data = renderData(random.nextInt(maxSize - minSize) + minSize, dataClazz, dataSupplier);
+            long id = 0;
+            RBT<T> rbt = new RBT<>(dataCmp);
+            HashMap<T, ArrayList<Long>> map = new HashMap<>();
+            TreeSet<Container> tree = new TreeSet<>((x, y) -> {
+                int t = dataCmp.compare(x.data, y.data);
+                return t == 0 ? Long.compare(x.id, y.id) : t;
+            });
+            for (T x : data) {
+                map.put(x, new ArrayList<>());
+            }
             try {
-                int ind = 0;
-                for (T x : rbt) {
-                    assert data[ind].equals(x) : data[ind] + ", " + x;
-                    ind++;
-                }
-                ind = 0;
-                boolean[] del = new boolean[data.length];
-                Iterator<T> it = rbt.iterator();
-                while (it.hasNext()) {
-                    it.next();
-                    if (random.nextBoolean()) {
-                        delInd.add(ind);
-                        del[ind] = true;
-                        it.remove();
+                for (int j = 0; j < roundCnt; j++) {
+                    for (T x : data) {
+                        rbt.insert(x);
+                        tree.add(new Container(x, id));
+                        ArrayList<Long> list = map.get(x);
+                        list.add(id);
+                        map.put(x, list);
+                        id++;
+                        addList.add(x);
+                        delList.add(null);
+                        if (random.nextInt(10) == 1) {
+                            break;
+                        }
                     }
-                    ind++;
-                }
-
-                it = rbt.iterator();
-                ind = 0;
-                while (it.hasNext()) {
-                    while (ind < del.length && del[ind]) {
-                        ind++;
+                    Iterator<T> it = rbt.iterator();
+                    for (Container x : tree) {
+                        assert it.hasNext();
+                        T t = it.next();
+                        assert x.data.equals(t) : x.data + ", " + t;
                     }
-                    assert ind < del.length : ind + ", " + data.length;
-                    T next = it.next();
-                    assert next.equals(data[ind]) : next + ", " + data[ind];
-                    ind++;
+                    it = rbt.iterator();
+                    while (it.hasNext()) {
+                        T x = it.next();
+                        if (random.nextBoolean()) {
+                            ArrayList<Long> list = map.get(x);
+                            long t = list.get(list.size() - 1);
+                            list.remove(list.size() - 1);
+                            tree.remove(new Container(x, t));
+                            it.remove();
+                            delList.add(x);
+                            addList.add(null);
+                        }
+                    }
+                    it = rbt.iterator();
+                    for (Container x : tree) {
+                        assert it.hasNext();
+                        T t = it.next();
+                        assert x.data.equals(t) : x.data + ", " + t;
+                    }
                 }
             } catch (Throwable throwable) {
-                System.out.println("delIndex: ");
-                System.out.println(delInd);
+                System.out.println("delList: ");
+                System.out.println(delList);
+                System.out.println("addList: ");
+                System.out.println(addList);
                 System.out.println("data: ");
                 System.out.println(Arrays.toString(data));
+                System.out.flush();
                 throw throwable;
             }
         }
@@ -82,7 +103,7 @@ public class RBTTest {
 
     @Test
     public void test1() {
-        test(Integer.class, () -> random.nextInt(), Comparator.comparingInt(x -> x));
+        test(Integer.class, () -> random.nextInt(10), Comparator.comparingInt(x -> x));
         test(Double.class, () -> random.nextDouble(), Comparator.comparingDouble(x -> x));
         test(TestClass.class, () -> new TestClass(random.nextInt()), Comparator.comparingInt(x -> x.n));
     }
